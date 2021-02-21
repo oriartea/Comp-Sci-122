4967d3a54dbc5001d444429d54bffc7e305ede45
oriartea <78041030+oriartea@users.noreply.github.com>
2021-02-21 00:54:27 -0800

    Add files via upload

---
+++ CheckSum.c
@@ -0,0 +1,49 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "<sys/types.h"
+#include <unistd.h>
+
+#define max_int (255)
+#define byte unsigned byte unsigned char 
+
+int main (argc, char * argv[], char ** envp) {
+   int count = 10;
+   int sum = 0;
+   int quotient;
+   int remainder;
+   byte checksum;
+   byte buffer[10];
+   
+   read(STDIN_FILENO, &buffer, 10);
+   
+      for (int x = 0; x < count; x++)  
+      {
+         printf("%d ", buffer[x]);
+         printf("\n"); 
+      }
+         for (int x = 0; x < count; x++)  
+          {
+            if (x == 5)     
+            {
+               checksum = buffer[x];
+               buffer[x] = 0;  
+            }
+            sum += buffer[x]; 
+          }
+         
+            quotient = sum / (max_int + 1);
+            remainder = sum % (max_int + 1);
+            sum = quotient + remainder;
+            complement = max_int - sum;
+            
+   /* the following is the protoype for the read system call */
+   /* int read(int fildes, void *buf, size_t nbyte); */
+fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+   if (checksum != complement ) {
+      fprintf(stderr, "Error Detected!\n");
+      return 1;
+   }
+   return 0;
+}      
+                               
+            
